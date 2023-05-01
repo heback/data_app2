042e5345a3dd5f2bfc652cf255f87f5163468d7e
heback <heback@gmail.com>
2023-05-01 15:36:54 +0900

    asdfghj

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.title('Hello World!')
+
+name = 'Jungu Lee'
+
+st.text(f'My name is {name}')
+
+st.markdown('This is **markdown**')
+
+import pandas as pd
+df = pd.read_csv('data/iris.csv')
+
+st.dataframe(df)
